8b8d84e9aee66fc2697a9329e524dfd5886545f9
Sagar Seth <lightningsagar0@gmail.com>
2026-05-08 22:15:43 +0530

    added the sample server to testt (finish)

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
